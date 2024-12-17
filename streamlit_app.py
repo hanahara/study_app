88b6198cf6e3c5e9e65563acdc08319331dae47e
hanahara <82866206+hanahara@users.noreply.github.com>
2024-12-18 08:43:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,11 @@ import sqlite3
 import requests
 
 PROXY = {
-    "https": "socks5://45.144.196.94:1080"
+    "https": "socks5h://socks.hide.me:1080"
 }
 response = requests.get("https://study01.streamlit.app/", proxies=PROXY)
 
-proxy_support = urllib.request.ProxyHandler(PROXY)
-# Create opener
-opener = urllib.request.build_opener(proxy_support)
-# Install opener
-urllib.request.install_opener(opener)
 
-webpage = urllib.request.urlopen(response)
 
 def save_to_db(question_data):
     conn = sqlite3.connect('questions.db')
