dfa012b4cc751996e95f76c36fb3ff09c8320309
hanahara <82866206+hanahara@users.noreply.github.com>
2024-12-18 07:51:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,10 @@ import os
 import sqlite3
 
 import requests
-proxies = {
+proxie = {
     "https": "socks5://socks.hide.me:1080"
 }
-response = requests.get('https://study01.streamlit.app',proxies=str("socks5://socks.hide.me:1080"))
+response = requests.get('https://study01.streamlit.app',proxies=proxie)
 print(response.text)
 
 def save_to_db(question_data):
