c6b97a3d114cc2833b63da3cad9ecd3e26caf334
hanahara <82866206+hanahara@users.noreply.github.com>
2024-12-18 05:38:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ import requests
 
 # プロキシの設定
 proxies = {
-    "http": "http://socks.hide.me:1080",
-    "https": "https://socks.hide.me:1080"
+    "http": "socks5://socks.hide.me:1080",
+    "https": "socks5://socks.hide.me:1080"
 }
 
 # リクエストを送る
