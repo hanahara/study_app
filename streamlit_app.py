2ad03ed8ed2d5f9a2be2e56eb65dfb94113b70f0
hanahara <82866206+hanahara@users.noreply.github.com>
2024-12-18 08:08:15 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,9 @@ from langchain.schema import SystemMessage, HumanMessage, AIMessage
 import os
 import sqlite3
 
-proxies =  "https//:socks5://socks.hide.me:1080"
+proxie =  "https//:socks5://socks.hide.me:1080"
 
+proxies = proxie
 
 
 
