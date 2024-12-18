a6381c39a1f2a4d5b0b983005329a2acf5ff57dd
hanahara <82866206+hanahara@users.noreply.github.com>
2024-12-18 09:55:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
 import os
 import sqlite3
-import requests
+#import requests
 
 #PROXY = {
 #    "https": "socks5://socks.hide.me:1080"
